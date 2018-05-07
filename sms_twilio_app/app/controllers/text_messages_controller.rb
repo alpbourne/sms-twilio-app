ad2134551f08017557035c35d8328089b40e6c51
alpbourne <alpbourne@gmail.com>
2018-05-07 12:58:26 -0400

    added to text_message index view page and text_messages controller

--- sms_twilio_app/app/controllers/text_messages_controller.rb
+++ sms_twilio_app/app/controllers/text_messages_controller.rb
@@ -1,7 +1,15 @@
 class TextMessagesController < ApplicationController
   def index
+    @text_message = TextMessage.new
+    @text_messages = TextMessage.all
   end
 
   def create
+    @text_message = TextMessage.new
+    @text_message.name = params[:text_message][:name]
+    @text_message.phone = params[:text_message][:phone]
+    @text_message.message = params[:text_message][:message]
+    @text_message.save
+    redirect_to text_messages_path
   end
 end
