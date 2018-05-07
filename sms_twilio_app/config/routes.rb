b677f6d1c9d38e8c6f49e565213fd984ef57131e
alpbourne <alpbourne@gmail.com>
2018-05-07 12:54:19 -0400

    fixed routes.rb and added index view page content

--- sms_twilio_app/config/routes.rb
+++ sms_twilio_app/config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
-  get 'text_messages/index'
-
-  get 'text_messages/create'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :text_messages, only: [:create, :index]
 end
