12fac308d4b7acd078bb528d1836b535f474aed5
alpbourne <alpbourne@gmail.com>
2018-05-07 12:50:36 -0400

    added text_messages controller and migrated database

---
+++ sms_twilio_app/app/controllers/text_messages_controller.rb
@@ -0,0 +1,7 @@
+class TextMessagesController < ApplicationController
+  def index
+  end
+
+  def create
+  end
+end
--- sms_twilio_app/config/routes.rb
+++ sms_twilio_app/config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  get 'text_messages/index'
+
+  get 'text_messages/create'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
