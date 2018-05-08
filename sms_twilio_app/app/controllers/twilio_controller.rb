d3602f8189efab4433b10ac60394ca1e952ef44d
alpbourne <alpbourne@gmail.com>
2018-05-08 11:13:22 -0400

    received messages are saving

--- sms_twilio_app/app/controllers/twilio_controller.rb
+++ sms_twilio_app/app/controllers/twilio_controller.rb
@@ -2,6 +2,10 @@ class TwilioController < ApplicationController
   skip_before_action :verify_authenticity_token
 
   def sms
+    phone = params[:From]
+    message = params[:Body]
+    name = "Me"
+    TextMessage.new(name: name, phone: phone, message: message).save
     # body = helpers.parse_sms(params)
     response = Twilio::TwiML::MessagingResponse.new do |r|
       r.message body: TextMessage.last.message
